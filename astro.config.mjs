3fa116f47a871f61574403d67f2ec32cc1a6b6c8
Martin08ABM <martin08abm@gmail.com>
2025-06-22 19:06:39 +0200

    Primer commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import clerk from "@clerk/astro";
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [clerk()],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+});
